089e7bf1cea3601371473a334b28353fbe90d2de
robbins312 <robbins312@naver.com>
2023-12-15 16:20:30 +0900

    done

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,32 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  //token 가져오기
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(() => {
+    // token 없으면 return
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPRIED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    // 한시간 지나면 logout ghcnf
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
